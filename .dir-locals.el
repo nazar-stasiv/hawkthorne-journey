91b7a2203fae454a34ce8e01240b35286bf7ae45
Nazar Stasiv <nazar@autistici.org>
2024-04-06 14:31:55 +0300

    add lua_modules to repl

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
-((fennel-mode . ((fennel-program . (concat "fennel --lua luajit --no-fennelrc --repl --add-package-path "
-					   (expand-file-name (ffip-get-project-root-directory))
-					   "lua_modules/share/lua/5.1/?.lua --add-package-path "
-					   (expand-file-name (ffip-get-project-root-directory))
-					   "lua_modules/share/lua/5.1/?/init.lua")))))
+((fennel-mode . ((eval . (setq fennel-program
+			       (let ((p (expand-file-name (ffip-get-project-root-directory))))
+				 (concat "fennel --add-package-path " p
+					 "lua_modules/share/lua/5.1/?.lua --add-package-path " p
+					 "lua_modules/share/lua/5.1/?/init.lua --lua luajit --repl")))))))
 
